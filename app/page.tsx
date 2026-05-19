05faea19eff9842fa3a0753bee5066871ea55ba2
Prabathunni <prabathunni826@gmail.com>
2026-05-19 12:23:37 +0530

    fix the folder structures

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import { scrollTo } from "./components/nav-data";
 import './page.css'
 
